354bb952ca0db4cea1b2ed3065f597f4c8b93645
avahoffman <avamariehoffman@gmail.com>
2023-06-04 09:54:19 -0400

    Read soil data and clean it up

--- R/getdata.R
+++ R/getdata.R
@@ -10,6 +10,9 @@ getdata <- function() {
     read_sheet(
       "https://docs.google.com/spreadsheets/d/1KrPJe9OOGuix2EAvcTfuspAe3kqN-y20Huyf5ImBEl4/edit#gid=804798874"
     )
+  # If you want to save and read..
+  # write.csv(soil_data, "soil_data.csv")
+  # soil_data <- read.csv("soil_data.csv")[,-1]
 
   # Remove parentheses, split column, fix negatives, and make numeric
   gps_points <-
@@ -35,4 +38,18 @@ getdata <- function() {
     pull()
 
   return(list(points = gps_points, sitenames = sitenames))
+}
+
+
+get_soil_data <- function(){
+  # https://data.imap.maryland.gov/datasets/9c48f92b2b4e4663aa78fdd64a1ab010
+  md_ssurgo1 <- rgdal::readOGR("data/soil_types/Maryland_SSURGO_Soils_-_SSURGO_Soils_Needwood_1.geojson")
+  md_ssurgo2 <- rgdal::readOGR("data/soil_types/Maryland_SSURGO_Soils_-_SSURGO_Soils_Needwood_2.geojson")
+  soil_type_data <- rbind(md_ssurgo1, md_ssurgo2)
+
+  soil_type_data$CLAY <- round(soil_type_data$CLAY, 1)
+  soil_type_data$SAND <- round(soil_type_data$SAND, 1)
+  soil_type_data$SILT <- round(soil_type_data$SILT, 1)
+
+  return(soil_type_data)
 }
