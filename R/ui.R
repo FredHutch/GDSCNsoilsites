0847b443a8537a91024a77db4405ff3a7aa4867c
avahoffman <avamariehoffman@gmail.com>
2023-08-08 15:30:54 -0400

    Revert everything

--- R/ui.R
+++ R/ui.R
@@ -6,12 +6,6 @@
 #' @examples
 shiny_ui <- function() {
   fluidPage(
-    checkboxInput(
-      "soil_geom_toggle",
-      label = 'Toggle Soil Data',
-      value = FALSE,
-      width = NULL
-    ),
     leafletOutput("soilmap", height = 800)
   )
 }
