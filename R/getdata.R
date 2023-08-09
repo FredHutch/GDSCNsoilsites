a788180564d2edf839ffaa9978e78a5ff05eb1f5
avahoffman <avamariehoffman@gmail.com>
2023-08-09 14:09:51 -0400

    Read in other sheet!

--- R/getdata.R
+++ R/getdata.R
@@ -27,9 +27,11 @@ do_gs4_auth <- function() {
 #' getdata()$points
 #' getdata()$sitenames
 getdata <- function() {
-  # Set filename and URL
-  google_sheet_url <-
+  # Set filename and URLs
+  google_sheet_url_1 <-
     "https://docs.google.com/spreadsheets/d/1KrPJe9OOGuix2EAvcTfuspAe3kqN-y20Huyf5ImBEl4/edit#gid=804798874"
+  google_sheet_url_2 <-
+    "https://docs.google.com/spreadsheets/d/1l7wuOt0DZp0NHMAriedbG4EJkl06Mh-G01CrVh8ySJE/edit#gid=804798874"
   soil_file <- "soil_data.csv"
 
   # Check if the file is/has been written recently.
@@ -37,7 +39,12 @@ getdata <- function() {
   if (!(file.exists(soil_file))) {
     do_gs4_auth()
     soil_data <-
-      read_sheet(google_sheet_url)
+      inner_join(
+        read_sheet(google_sheet_url_1),
+        read_sheet(google_sheet_url_2),
+        by = c(`What is your site name?` = 'site_name'),
+        keep = TRUE
+      )
     write.csv(soil_data, soil_file)
   } else {
     last_created <- file.info(soil_file)$ctime
@@ -46,7 +53,12 @@ getdata <- function() {
     if (time_since > lubridate::as.difftime(24, units = "hours")) {
       do_gs4_auth()
       soil_data <-
-        read_sheet(google_sheet_url)
+        inner_join(
+          read_sheet(google_sheet_url_1),
+          read_sheet(google_sheet_url_2),
+          by = c(`What is your site name?` = 'site_name'),
+          keep = TRUE
+        )
       write.csv(soil_data, soil_file)
     } else {
       soil_data <- read.csv(soil_file)[, -1]
@@ -77,7 +89,14 @@ getdata <- function() {
     select(2) %>%
     pull()
 
-  return(list(points = gps_points, sitenames = sitenames))
+  # Adjust image urls
+  image_urls <-
+    soil_data %>%
+    dplyr::mutate(url = paste0('https://drive.google.com/uc?export=view&id=', google_img_id)) %>%
+    select(url) %>%
+    pull()
+
+  return(list(points = gps_points, sitenames = sitenames, image_urls = image_urls))
 }
 
 
