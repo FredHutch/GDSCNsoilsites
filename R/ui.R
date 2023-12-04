bb75ec7ec8b0b2b414bec508662a380624142402
avahoffman <avamariehoffman@gmail.com>
2023-12-04 15:55:40 -0500

    Revert "Reorg side menu"
    This reverts commit 5b75e855c1d840eb78144c58e1be162ba5ed8173.

--- R/ui.R
+++ R/ui.R
@@ -31,11 +31,18 @@ shiny_ui <- function() {
 
       skin = "green",
 
-      dashboardHeader(title="BioDIGS 🧬🦠", titleWidth = 300),
+      dashboardHeader(title="🧬 BioDIGS 🦠", titleWidth = 300),
 
       dashboardSidebar(
         width = 300,
         sidebarMenu(
+          HTML(
+            paste0(
+              "<br>",
+              "<a href='https://gdscn.org' target='_blank'><img style = 'display: block; margin-left: auto; margin-right: auto;' src='https://raw.githubusercontent.com/jhudsl/AnVIL_Template/main/assets/GDSCN_style/logo-gdscn.png' width = '186'></a>",
+              "<br>"
+            )
+          ),
           menuItem(
             "About",
             tabName = "about",
@@ -75,14 +82,7 @@ shiny_ui <- function() {
             "FAQs",
             tabName = "faq",
             icon = icon("lightbulb")
-          ),
-          HTML(
-            paste0(
-              "<br>",
-              "<a href='https://gdscn.org' target='_blank'><img style = 'display: block; margin-left: auto; margin-right: auto;' src='https://raw.githubusercontent.com/jhudsl/AnVIL_Template/main/assets/GDSCN_style/logo-gdscn.png' width = '186'></a>",
-              "<br>"
-            )
-          ),
+          )
         )
 
       ), # end dashboardSidebar
