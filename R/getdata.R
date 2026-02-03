fbb6f134c359caa2d18928ba5c5bc8e2ed15974d
avahoffman <avamariehoffman@gmail.com>
2026-02-03 11:49:31 -0500

    Update snapshot used

--- R/getdata.R
+++ R/getdata.R
@@ -121,7 +121,7 @@ scrubseqdata <- function(infile, outfile) {
 #' @examples
 #' getdata()
 #' getdata(dataset = "soil")
-getdata <- function(dataset = "sites", snapshot = "20260108") {
+getdata <- function(dataset = "sites", snapshot = "20260203") {
 
   if(dataset == "sites") {
     snapshot_path <- paste0("data/snapshots/BioDIGS_sites_", snapshot,".csv")
